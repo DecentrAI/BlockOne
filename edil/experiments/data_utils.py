f955d54752519a390fc206b7909d3545d077ae15
Andrei Ionut Damian <andrei@lummetry.ai>
2022-02-03 16:57:34 +0200

    update main EDIL test

--- edil/experiments/data_utils.py
+++ edil/experiments/data_utils.py
@@ -24,7 +24,9 @@ Copyright 2019-2021 Lummetry.AI (Knowledge Investment Group SRL). All Rights Res
 import torch as th
 import torchvision as tv
 
-def get_mnist_data(dev):
+def get_mnist_data(dev=None, as_numpy=False):
+  if dev is None:
+    dev = th.device('cuda')
   
   train = tv.datasets.MNIST(
     root='_cache', 
@@ -52,5 +54,7 @@ def get_mnist_data(dev):
   x_test = (full_test.data[n_dev:].view(-1,1,28,28) / 255.).to(dev)
   y_test = full_test.targets[n_dev:].to(dev)
 
+  if as_numpy:
+    return (x_train.cpu().numpy(), y_train.cpu().numpy()), (x_dev.cpu().numpy(), y_dev.cpu().numpy()), (x_test.cpu().numpy(), y_test.cpu().numpy())
   return (x_train, y_train), (x_dev, y_dev), (x_test, y_test)
   
--- edil/experiments/local_test.py
+++ edil/experiments/local_test.py
@@ -20,10 +20,32 @@ Copyright 2019-2021 Lummetry.AI (Knowledge Investment Group SRL). All Rights Res
 @description:
 @created on: Thu Feb  3 14:41:59 2022
 @created by: damia
+
+
+Algorithm:
+  1. Get data
+  2. Create multiple nodes
+  3. Get a arbitrary node M and consider it as data owner
+  4. Prepare M to have domain encoder and a model definition
+  5. Send distributed job to other workers recording input vs received data size
+  6. Monitor individual training results and overall results
+  7. Finally aggregate domain + aggregated and test it
+
 """
 
 from edil.node import ProcessingNode
 
+from edil.experiments.data_utils import get_mnist_data
+
 
 if __name__ == '__main__':
+  
+  
+  
+  (x_train, y_train), (x_dev, y_dev), (x_test, y_test) = get_mnist_data(as_numpy=True)
+  
+  w1 = ProcessingNode()
+  w2 = ProcessingNode()
+  w3 = ProcessingNode()
+  w4 = ProcessingNode()
   
