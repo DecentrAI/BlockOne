57fcc4e0208c7632e741134994d4150114976c7c
Andrei Ionut Damian <andrei.damian@me.com>
2024-04-08 08:50:10 +0300

    deleted auto-completed docstrings

--- edil/th_utils.py
+++ edil/th_utils.py
@@ -1,23 +1,6 @@
 # -*- coding: utf-8 -*-
 """
-Copyright 2019-2021 Lummetry.AI (Knowledge Investment Group SRL). All Rights Reserved.
 
-
-* NOTICE:  All information contained herein is, and remains
-* the property of Knowledge Investment Group SRL.  
-* The intellectual and technical concepts contained
-* herein are proprietary to Knowledge Investment Group SRL
-* and may be covered by Romanian and Foreign Patents,
-* patents in process, and are protected by trade secret or copyright law.
-* Dissemination of this information or reproduction of this material
-* is strictly forbidden unless prior written permission is obtained
-* from Knowledge Investment Group SRL.
-
-
-@copyright: Lummetry.AI
-@author: Lummetry.AI
-@project: 
-@description:
 @created on: Tue Feb  1 08:57:01 2022
 @created by: damia
 """
--- eth_test.py
+++ eth_test.py
@@ -1,22 +1,4 @@
-# -*- coding: utf-8 -*-
-"""
-Copyright 2017-2021 Lummetry.AI (Knowledge Investment Group SRL). All Rights Reserved.
-* NOTICE:  All information contained herein is, and remains
-* the property of Knowledge Investment Group SRL.
-* The intellectual and technical concepts contained
-* herein are proprietary to Knowledge Investment Group SRL
-* and may be covered by Romanian and Foreign Patents,
-* patents in process, and are protected by trade secret or copyright law.
-* Dissemination of this information or reproduction of this material
-* is strictly forbidden unless prior written permission is obtained
-* from Knowledge Investment Group SRL.
 
-@copyright: Lummetry.AI
-@author: Lummetry.AI - AID
-@project:
-@description:
-
-"""
 import os
 
 # os.environ["WEB3_INFURA_PROJECT_ID"] = "81f3ba2b95ff4056bb899598f6170f33"
